8301478d98e635027af92ab7e80125ee47fd77ab
4SAMU <samuelnzomo4@gmail.com>
2022-05-17 15:06:02 +0300

    first commit

---
+++ selection_control_exercise/selection_control_exercise.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace selection_control_exercise
+{
+    partial class selection_control_exercise
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtcomboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtWebsite = new System.Windows.Forms.RadioButton();
+            this.txtEmail = new System.Windows.Forms.RadioButton();
+            this.txtFamilyFriends = new System.Windows.Forms.RadioButton();
+            this.txtFlyers = new System.Windows.Forms.RadioButton();
+            this.txtExperienced = new System.Windows.Forms.RadioButton();
+            this.txtnew = new System.Windows.Forms.RadioButton();
+            this.txtDisplay = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDISPLAY = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "HOURS/WEEK";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "STATUS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(23, 337);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(256, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "WHERE DID YOU HERE US FROM?";
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtname.Location = new System.Drawing.Point(152, 92);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(241, 22);
+            this.txtname.TabIndex = 5;
+            // 
+            // txtcomboBox1
+            // 
+            this.txtcomboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcomboBox1.FormattingEnabled = true;
+            this.txtcomboBox1.Items.AddRange(new object[] {
+            "5-10",
+            "11-20",
+            "21-30",
+            "31-40"});
+            this.txtcomboBox1.Location = new System.Drawing.Point(153, 238);
+            this.txtcomboBox1.Name = "txtcomboBox1";
+            this.txtcomboBox1.Size = new System.Drawing.Size(240, 24);
+            this.txtcomboBox1.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(989, 74);
+            this.panel1.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(293, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(270, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "SELECTION CONTROL APPLICATION";
+            // 
+            // txtWebsite
+            // 
+            this.txtWebsite.AutoSize = true;
+            this.txtWebsite.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWebsite.Location = new System.Drawing.Point(11, 110);
+            this.txtWebsite.Name = "txtWebsite";
+            this.txtWebsite.Size = new System.Drawing.Size(227, 20);
+            this.txtWebsite.TabIndex = 17;
+            this.txtWebsite.TabStop = true;
+            this.txtWebsite.Text = "WEBSITE ADVERTISEMENT";
+            this.txtWebsite.UseVisualStyleBackColor = true;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.AutoSize = true;
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(11, 79);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(203, 20);
+            this.txtEmail.TabIndex = 16;
+            this.txtEmail.TabStop = true;
+            this.txtEmail.Text = "EMAIL ADVERTISEMENT";
+            this.txtEmail.UseVisualStyleBackColor = true;
+            // 
+            // txtFamilyFriends
+            // 
+            this.txtFamilyFriends.AutoSize = true;
+            this.txtFamilyFriends.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFamilyFriends.Location = new System.Drawing.Point(11, 43);
+            this.txtFamilyFriends.Name = "txtFamilyFriends";
+            this.txtFamilyFriends.Size = new System.Drawing.Size(167, 20);
+            this.txtFamilyFriends.TabIndex = 15;
+            this.txtFamilyFriends.TabStop = true;
+            this.txtFamilyFriends.Text = "FAMILY or FRIENDS";
+            this.txtFamilyFriends.UseVisualStyleBackColor = true;
+            // 
+            // txtFlyers
+            // 
+            this.txtFlyers.AutoSize = true;
+            this.txtFlyers.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFlyers.Location = new System.Drawing.Point(11, 3);
+            this.txtFlyers.Name = "txtFlyers";
+            this.txtFlyers.Size = new System.Drawing.Size(84, 20);
+            this.txtFlyers.TabIndex = 14;
+            this.txtFlyers.TabStop = true;
+            this.txtFlyers.Text = "FLYERS";
+            this.txtFlyers.UseVisualStyleBackColor = true;
+            // 
+            // txtExperienced
+            // 
+            this.txtExperienced.AutoSize = true;
+            this.txtExperienced.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtExperienced.Location = new System.Drawing.Point(253, 289);
+            this.txtExperienced.Name = "txtExperienced";
+            this.txtExperienced.Size = new System.Drawing.Size(132, 20);
+            this.txtExperienced.TabIndex = 27;
+            this.txtExperienced.TabStop = true;
+            this.txtExperienced.Text = "EXPERIENCED";
+            this.txtExperienced.UseVisualStyleBackColor = true;
+            // 
+            // txtnew
+            // 
+            this.txtnew.AutoSize = true;
+            this.txtnew.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnew.Location = new System.Drawing.Point(151, 289);
+            this.txtnew.Name = "txtnew";
+            this.txtnew.Size = new System.Drawing.Size(61, 20);
+            this.txtnew.TabIndex = 26;
+            this.txtnew.TabStop = true;
+            this.txtnew.Text = "NEW";
+            this.txtnew.UseVisualStyleBackColor = true;
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDisplay.Location = new System.Drawing.Point(535, 92);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(437, 384);
+            this.txtDisplay.TabIndex = 28;
+            this.txtDisplay.Text = "";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtFlyers);
+            this.panel2.Controls.Add(this.txtFamilyFriends);
+            this.panel2.Controls.Add(this.txtEmail);
+            this.panel2.Controls.Add(this.txtWebsite);
+            this.panel2.Location = new System.Drawing.Point(285, 337);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(244, 139);
+            this.panel2.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "EVENTS";
+            // 
+            // btnDISPLAY
+            // 
+            this.btnDISPLAY.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnDISPLAY.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDISPLAY.Location = new System.Drawing.Point(636, 492);
+            this.btnDISPLAY.Name = "btnDISPLAY";
+            this.btnDISPLAY.Size = new System.Drawing.Size(253, 48);
+            this.btnDISPLAY.TabIndex = 30;
+            this.btnDISPLAY.Text = "DISPLAY";
+            this.btnDISPLAY.UseVisualStyleBackColor = false;
+            this.btnDISPLAY.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "BRAT FRY",
+            "WALKTHROUGH",
+            "GOLFING",
+            "BAKE SALE"});
+            this.checkedListBox1.Location = new System.Drawing.Point(152, 138);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(233, 72);
+            this.checkedListBox1.TabIndex = 31;
+            // 
+            // selection_control_exercise
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(989, 547);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.btnDISPLAY);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.txtExperienced);
+            this.Controls.Add(this.txtnew);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtcomboBox1);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "selection_control_exercise";
+            this.Text = "SELECTION CONTROL EXERCISE";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.ComboBox txtcomboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton txtWebsite;
+        private System.Windows.Forms.RadioButton txtEmail;
+        private System.Windows.Forms.RadioButton txtFamilyFriends;
+        private System.Windows.Forms.RadioButton txtFlyers;
+        private System.Windows.Forms.RadioButton txtExperienced;
+        private System.Windows.Forms.RadioButton txtnew;
+        private System.Windows.Forms.RichTextBox txtDisplay;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDISPLAY;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ selection_control_exercise/selection_control_exercise.cs
@@ -0,0 +1,66 @@
+﻿//programmer name: Prathyusha Pinnintti
+//project number: ch6 Exercise
+//Description:selection control exercise
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace selection_control_exercise
+{
+    public partial class selection_control_exercise : Form
+    {
+        public selection_control_exercise()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Clear();
+
+            txtDisplay.AppendText(Environment.NewLine);
+            txtDisplay.AppendText("\tNAME:\t\t" + txtname.Text + Environment.NewLine);
+            //check events
+            txtDisplay.AppendText("\tEVENTS:\t\t" + Environment.NewLine);
+            foreach (string item in checkedListBox1.CheckedItems)
+            {
+                txtDisplay.AppendText("\t\t\t"+item + Environment.NewLine);
+            }
+           
+            // status
+            if (txtnew.Checked)
+            {
+                txtDisplay.AppendText("\tSTATUS:\t\t" + txtnew.Text + Environment.NewLine);
+            }
+            else if (txtExperienced.Checked)
+            {
+                txtDisplay.AppendText("\tSTATUS:\t\t" + txtExperienced.Text + Environment.NewLine);
+
+            }
+            //where you heard us from
+            if (txtFlyers.Checked)
+            {
+                txtDisplay.AppendText("\twhere you heard us from:\t" + txtFlyers.Text + Environment.NewLine);
+            }
+            else if (txtFamilyFriends.Checked)
+            {
+                txtDisplay.AppendText("\twhere you heard us from:\t" + txtFamilyFriends.Text + Environment.NewLine);
+            }
+            else if (txtEmail.Checked)
+            {
+                txtDisplay.AppendText("\twhere you heard us from:\t" + txtEmail.Text + Environment.NewLine);
+            }
+            else if (txtWebsite.Checked)
+            {
+                txtDisplay.AppendText("\twhere you heard us from:\t" + txtWebsite.Text + Environment.NewLine);
+            }
+        }
+    }
+}
